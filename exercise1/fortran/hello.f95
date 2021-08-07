ef314cbefbbd2f3653853ef58b58779f2be6b813
Carlos Henrique <carloshenriquefbf@poli.ufrj.br>
2021-08-07 00:59:10 -0300

    :sparkles: add fortran files

---
+++ exercise1/fortran/hello.f95
@@ -0,0 +1,125 @@
+program hello
+implicit none
+
+integer size, n
+real*8, dimension(:), allocatable :: vector, resultIJ, resultJI
+real*8, dimension(:,:), allocatable :: matrix
+real start_t, end_t, total_t
+call random_seed()
+
+open(1, file = '../documents/fortran/csv/matrixVectorMultiplicationIJ.csv', action = 'write')
+open(2, file = '../documents/fortran/csv/matrixVectorMultiplicationJI.csv', action = 'write')
+
+do n = 1, 5, 1
+    size = n*5000
+    allocate(vector(size))
+    allocate(matrix(size,size))
+
+    call fillVector(vector,size)
+    call fillMatrix(matrix,size)
+
+    allocate(resultIJ(size))
+    call fillResultsVector(resultIJ,size)
+
+    call cpu_time(start_t)
+    call matrixVectorMultiplicationIJ(matrix,vector,resultIJ,size)
+    call cpu_time(end_t)
+
+    total_t =  end_t - start_t
+    print *, 'It took the computer', total_t, 'to compute a ',size,'degree matrix on IJ; i = ',n
+    write(1,*) size,";",total_t
+    deallocate(resultIJ)
+
+    end_t = 0
+    start_t = 0
+    total_t = 0
+
+    allocate(resultJI(size))
+    call fillResultsVector(resultJI,size)
+
+    call cpu_time(start_t )
+    call matrixVectorMultiplicationJI(matrix,vector,resultJI,size)
+    call cpu_time(end_t )
+
+    total_t  =  end_t  - start_t
+    print *, "It took the computer", total_t, "to compute a ",size,"degree matrix on JI; i = ",n
+    write(2,*) size,";",total_t
+    deallocate(resultJI)
+
+    deallocate(matrix)
+    deallocate(vector)
+end do
+
+close(1)
+close(2)
+
+contains
+
+subroutine fillVector(vector, size)
+    implicit none
+
+    real*8, dimension(:) :: vector
+    integer :: size, i
+    real*8 :: u
+
+    do i = 1, size
+            call random_number(u)
+            vector(i) = u
+    end do
+end
+
+subroutine fillResultsVector(vector, size)
+    implicit none
+
+    real*8, dimension(:) :: vector
+    integer :: size, i
+
+    do i = 1, size
+            vector(i) = 0
+    end do
+end
+
+subroutine fillMatrix(matrix, size)
+    implicit none
+
+    real*8, dimension(:,:) :: matrix
+    integer :: size, i, j
+    real*8 :: u
+
+    do i = 1, size
+        do j = 1, size
+            call random_number(u)
+            matrix(i, j) = u
+        end do
+    end do
+end
+
+subroutine matrixVectorMultiplicationIJ(matrix, vector, result, size)
+    implicit none
+
+    real*8, dimension(:,:) :: matrix
+    real*8, dimension(:) :: vector, result
+    integer :: size, i, j
+
+    do i = 1, size
+        do j = 1, size
+            result(i) = result(i) + (matrix(i, j) * vector(j))
+        end do
+    end do
+end
+
+subroutine matrixVectorMultiplicationJI(matrix, vector, result, size)
+    implicit none
+
+    real*8, dimension(:,:) :: matrix
+    real*8, dimension(:) :: vector, result
+    integer :: size, i, j
+
+    do j = 1, size
+        do i = 1, size
+            result(i) = result(i) + (matrix(i, j) * vector(j))
+        end do
+    end do
+end
+
+end program hello
--- exercise1/python/graph.py
+++ exercise1/python/graph.py
@@ -1,13 +1,26 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-IJ = pd.read_csv("../documents/c/csv/matrixVectorMultiplicationIJ.csv",header = None)
-JI = pd.read_csv("../documents/c/csv/matrixVectorMultiplicationJI.csv",header = None)
+C_IJ = pd.read_csv("../documents/c/csv/matrixVectorMultiplicationIJ.csv",header = None)
+C_JI = pd.read_csv("../documents/c/csv/matrixVectorMultiplicationJI.csv",header = None)
 
-plt.plot(IJ[0], IJ[1], label='I x J')
-plt.plot(JI[0], JI[1], label='J x I')
+plt.figure(1)
+plt.plot(C_IJ[0], C_IJ[1], label='I x J')
+plt.plot(C_JI[0], C_JI[1], label='J x I')
 plt.title('C')
 plt.ylabel('Runtime (s)')
 plt.xlabel('N')
 plt.legend()
 plt.savefig('../documents/c/images/graph.png')
+
+FORTRAN_IJ = pd.read_csv("../documents/fortran/csv/matrixVectorMultiplicationIJ.csv",header = None,  delimiter=';')
+FORTRAN_JI = pd.read_csv("../documents/fortran/csv/matrixVectorMultiplicationJI.csv",header = None,  delimiter=';')
+
+plt.figure(2)
+plt.plot(FORTRAN_IJ[0], FORTRAN_IJ[1], label='I x J')
+plt.plot(FORTRAN_JI[0], FORTRAN_JI[1], label='J x I')
+plt.title('Fortran')
+plt.ylabel('Runtime (s)')
+plt.xlabel('N')
+plt.legend()
+plt.savefig('../documents/fortran/images/graph.png')
